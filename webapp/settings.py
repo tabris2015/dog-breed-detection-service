ffb1b2c422ce533c3f739e3bde2e3206c565e840
Jose Laruta <eduardo.laruta@gmail.com>
2020-10-23 23:43:47 -0400

    base detection app

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')79om!%3(@+r4mpa@rt9qt@f^3#q%#^$lgc!8a1o+xj&#n(uo7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
