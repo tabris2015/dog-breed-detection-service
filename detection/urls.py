01d867fda85e8158cbd9eef2b2e0cc6b4f59849f
Jose Laruta <eduardo.laruta@gmail.com>
2020-11-14 02:18:46 -0400

    receive image from frontend

--- detection/urls.py
+++ detection/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from django.conf.urls import include
+from django.http import StreamingHttpResponse
 from . import views
+
+from . import services
 
 app_name = 'detection'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cv-camera/', lambda r: StreamingHttpResponse(services.gen(services.VideoCamera()),
+                                                       content_type='multipart/x-mixed-replace; boundary=frame')),
+    path('image/', views.receiveImage, name='receiveImage')
 ]
--- detection/views.py
+++ detection/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .utils import get_image_from_data_url
 # Create your views here.
 def index(request):
     """Pagina de inicio"""
     return render(request, 'detection/index.html')
 
+@csrf_exempt
+def receiveImage(request):
+    # print(type(request.body))
+    image_file = get_image_from_data_url(request.body)[0]
+    print(type(image_file))
+    return HttpResponse(status=200)
